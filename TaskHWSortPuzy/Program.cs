0288bd15a4648f7bea8d7721686e76ca595b715c
Aleksandr <maivan007@yandex.ru>
2022-07-26 15:05:23 +0400

    Add Task HW

---
+++ TaskHWSortPuzy/Program.cs
@@ -0,0 +1,65 @@
+﻿void Zadacha54()
+{
+    int rows = 4;
+    int columns = 7;
+    int[,] array = new int[rows, columns];
+
+    FillArray(array);
+    Console.WriteLine("Вывод массива: ");
+    PrintArray(array);
+
+    int min = array[0, 0];
+    int i_min = 0;
+    int j_min = 0;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            for (int k = 0; k < columns - 1 - j; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k];
+                    array[i, k] = array[i, k + 1];
+                    array[i, k + 1] = temp;
+                }
+            }
+        }
+    }
+
+    Console.WriteLine("Вывод нового массива: ");
+    PrintArray(array);
+}
+
+
+
+void FillArray(int[,] array)
+{
+    Random random = new Random();
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = random.Next(-10, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+Zadacha54();
