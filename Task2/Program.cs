181d890dea2df8ba3994cbd373d7effd227bc973
Aleksandr <maivan007@yandex.ru>
2022-07-27 16:02:07 +0400

    Add Task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу,
+// которая выведет все натуральные числа в промежутке от M до N.
+
+void Zadacha65()
+{
+    Console.Write("Введите первое число промежутка: ");
+    int numbM = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите второе число промежутка: ");
+    int numbN = Convert.ToInt32(Console.ReadLine());
+    Recursion(numbM, numbN);
+
+    void Recursion(int numbM, int numbN)
+    {
+        if (numbM > numbN) return;
+        Console.Write(numbM + " ");
+        numbM++;
+        Recursion(numbM, numbN);
+    }
+}
+
+Zadacha65();
