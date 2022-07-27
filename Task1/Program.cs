08b193f201d47bb225d3453a27bf6597c7abcdbf
Aleksandr <maivan007@yandex.ru>
2022-07-27 16:01:36 +0400

    Add Task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿void Zadachd63()
+{
+    int number = 15;
+    int count = 1;
+
+    Recursion(count, number);
+
+    void Recursion(int count, int number)
+    {
+        if (count > number) return;
+        Console.WriteLine(count + " ");
+        count++;
+        Recursion(count, number);
+    }
+
+}
+
+Zadachd63();
