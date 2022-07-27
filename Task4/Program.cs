23914e8c2ac31f3dbc45663eeaaabf5a2969489d
Aleksandr <maivan007@yandex.ru>
2022-07-27 16:04:18 +0400

    Add task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,22 @@
+﻿void Zadacha69()
+{
+    int a = 5;
+    int b = 4;
+    Recursion(a, b);
+
+    void Recursion(int a, int b, int result = 1)
+    {
+        if (b == 0)
+        {
+            Console.WriteLine(result);
+            return;
+        }
+
+        result *= a;
+        b--;
+        Recursion(a,b,result);
+
+    }
+}
+
+Zadacha69();
